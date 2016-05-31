de4d23ec597184b1cb47c77f6a3baedc83254be8
jasminsander <jasmin.sander@werum.com>
2016-05-31 19:48:55 +0200

    Ob das funktioniert...

--- src/turing_maschine/TM.java
+++ src/turing_maschine/TM.java
@@ -2,7 +2,9 @@ package turing_maschine;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TM {
 	final char LEER_FELD = '#';
@@ -11,11 +13,15 @@ public class TM {
 	List<Character> bandsymbole= new ArrayList<Character>();
 	Zustand startzustand;
 	List<Zustand> endzustaende = new ArrayList<Zustand>();
-	String eingabeband;
-	String startkonfiguration = startzustand + eingabeband;
+	Map<String, String> konfigurationen = new HashMap<String, String>();
 	
 	public void initialisiere(File tm_datei) {
-		TM_Parser.parse(tm_datei);
+		zustaende = TM_Parser.parse_zustaende(tm_datei);
+		eingabesymbole = TM_Parser.parse_eingabesymbole(tm_datei);
+		bandsymbole = TM_Parser.parse_bandsymbole(tm_datei);
+		startzustand = TM_Parser.parse_startzustand(tm_datei);
+		endzustaende = TM_Parser.parse_endzustaende(tm_datei);
+		konfigurationen = TM_Parser.parse_konfiguration(tm_datei);
 	}
 	
 	public void naechste_konfiguration() {
--- src/turing_maschine/TM_Parser.java
+++ src/turing_maschine/TM_Parser.java
@@ -1,11 +1,45 @@
 package turing_maschine;
 
 import java.io.File;
+import java.util.List;
+import java.util.Map;
 
 public class TM_Parser {
 
-	public static void parse(File tm_datei) {
+
+	public static List<Zustand> parse_zustaende(File tm_datei) {
+		return null;
 		// TODO Auto-generated method stub
+		
+	}
+
+	public static List<Character> parse_eingabesymbole(File tm_datei) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static List<Character> parse_bandsymbole(File tm_datei) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static Zustand parse_startzustand(File tm_datei) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static List<Zustand> parse_endzustaende(File tm_datei) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static Map<String, String> parse_konfiguration(File tm_datei) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
